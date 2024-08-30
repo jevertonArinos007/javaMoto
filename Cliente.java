4df5288c08791191f4aaa49360e12747805d24e4
jeverton arinos <jevertonarinos@gmail.com>
2024-08-30 18:10:26 -0300

    Subindo arquivo java solicitado

---
+++ Cliente.java
@@ -0,0 +1,44 @@
+package Moto;
+
+public class Cliente {
+
+	public Cliente() {
+		// TODO Auto-generated constructor stub
+	}
+        
+	   public int valor;
+	   
+	   public String nome;
+	   
+	   public String endereco;
+
+	public int getValor() {
+		return valor;
+	}
+
+	public void setValor(int valor) {
+		this.valor = valor;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+		public void nomeCleinte(String nome) {
+			setNome(nome);
+		}
+	
+	}
+
+
---
+++ Moto.java
@@ -0,0 +1,18 @@
+package Moto;
+
+
+/**@see Comentario exemplo 
+ * @author Jeverton Arinos
+ */
+public class Moto {
+	
+	
+	public static void main(String[] args) {
+			System.out.print("Ola ");
+			
+		   Moto Moto = new Moto(); 
+		  
+		
+	}
+
+}
